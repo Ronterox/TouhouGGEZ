e7428334b52c491ee1f7648607a371365962dd16
Ricardo <ricarditomontserrat@gmail.com>
2024-06-16 17:42:01 +0200

    Generalized movable, and can shoot bullets

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,37 @@
+mod physics;
+
 use ggez::{graphics::Color, input::keyboard::KeyCode, *};
-use mint::Point2;
+use mint::{Point2, Vector2};
+use physics::{Movable, Rigidbody};
+
+type Bullet = Entity;
+type Player = Entity;
+
+struct Entity {
+    rigidbody: Rigidbody,
+    sprite: graphics::Image,
+}
 
 struct State {
-    x: f32,
-    y: f32,
-    speed: f32,
-    sprite: graphics::Image,
+    player: Player,
+    bullets: Vec<Bullet>,
+}
+
+impl Movable for Entity {
+    fn get_rigidbody(&self) -> &Rigidbody {
+        &self.rigidbody
+    }
+
+    fn set_position(&mut self, x: f32, y: f32) {
+        self.rigidbody.position.x = x;
+        self.rigidbody.position.y = y;
+    }
 }
 
 impl State {
     fn if_press_move(&mut self, ctx: &Context, key: KeyCode, dir: (f32, f32)) {
         if ctx.keyboard.is_key_pressed(key) {
-            self.x += dir.0 * self.speed;
-            self.y += dir.1 * self.speed;
+            self.player.move_by(dir.0, dir.1);
         }
     }
 }
@@ -24,6 +43,10 @@ impl ggez::event::EventHandler<GameError> for State {
         self.if_press_move(&ctx, KeyCode::A, (-1.0, 0.0));
         self.if_press_move(&ctx, KeyCode::D, (1.0, 0.0));
 
+        self.bullets.iter_mut().for_each(|bullet| {
+            bullet.move_by(0.0, -1.0);
+        });
+
         Ok(())
     }
 
@@ -31,14 +54,26 @@ impl ggez::event::EventHandler<GameError> for State {
         let mut canvas = graphics::Canvas::from_frame(ctx, Color::BLACK);
 
         canvas.draw(
-            &self.sprite,
+            &self.player.sprite,
             graphics::DrawParam::new()
                 .dest(Point2 {
-                    x: self.x,
-                    y: self.y,
+                    x: self.player.x(),
+                    y: self.player.y(),
                 })
                 .scale(Point2 { x: 0.2, y: 0.2 }),
         );
+
+        self.bullets.iter().for_each(|bullet| {
+            canvas.draw(
+                &bullet.sprite,
+                graphics::DrawParam::new()
+                    .dest(Point2 {
+                        x: bullet.x(),
+                        y: bullet.y(),
+                    })
+                    .scale(Point2 { x: 0.2, y: 0.2 }),
+            );
+        });
 
         canvas.finish(ctx)
     }
@@ -51,11 +86,28 @@ fn main() -> GameResult {
         .build()
         .unwrap();
 
+    let mut bullets = Vec::new();
+    let mut y = 0.0;
+    for _ in 0..10 {
+        bullets.push(Entity {
+            rigidbody: Rigidbody {
+                position: Vector2 { x: 300., y: 600. - y },
+                speed: 5.0,
+            },
+            sprite: graphics::Image::from_path(&ctx, "/isaaac.jpg")?,
+        });
+        y += 200.0;
+    }
+
     let state = State {
-        x: 0.0,
-        y: 0.0,
-        speed: 10.0,
-        sprite: graphics::Image::from_path(&ctx, "/sakuya.png")?,
+        player: Entity {
+            rigidbody: Rigidbody {
+                position: Vector2 { x: 100.0, y: 100.0 },
+                speed: 5.0,
+            },
+            sprite: graphics::Image::from_path(&ctx, "/sakuya.png")?,
+        },
+        bullets,
     };
 
     event::run(ctx, event_loop, state);
---
+++ src/physics.rs
@@ -0,0 +1,27 @@
+use ggez::mint::Vector2;
+
+pub trait Movable {
+    fn get_rigidbody(&self) -> &Rigidbody;
+    fn set_position(&mut self, x: f32, y: f32);
+    fn speed(&self) -> f32 {
+        self.get_rigidbody().speed
+    }
+    fn position(&self) -> &Vector2<f32> {
+        &self.get_rigidbody().position
+    }
+    fn x(&self) -> f32 {
+        self.position().x
+    }
+    fn y(&self) -> f32 {
+        self.position().y
+    }
+    fn move_by(&mut self, x: f32, y: f32) {
+        let spd = self.speed();
+        self.set_position(self.x() + x * spd, self.y() + y * spd);
+    }
+}
+
+pub struct Rigidbody {
+    pub position: Vector2<f32>,
+    pub speed: f32,
+}
